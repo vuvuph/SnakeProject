790d10c4a43313da14ab88e9a1952d87c1d9e2f8
vuvuph <vu8436ph-s@student.lu.se>
2024-09-09 21:27:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Snake;
+using System;
+using System.Windows.Forms;
+
+namespace SnakeProject
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SnakeForm());
+        }
+    }
+}
